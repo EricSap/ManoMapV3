580114f576ae2cd0bf29178a222324e9b1a867d4
Renzo <renzo.vanaudenhoven@student.ucll.be>
2024-05-22 10:59:48 +0200

    view last commit

--- ManoMap3/patternDetectionScreen/patternDetectionSettings.py
+++ ManoMap3/patternDetectionScreen/patternDetectionSettings.py
@@ -1,8 +1,8 @@
 import customtkinter as ctk
 
 def create_settings_frame(root):
-    sensors_frame = ctk.CTkFrame(root)
-    sensors_frame.pack(pady=10, padx=10, fill="both", expand=True)
+    settings_frame = ctk.CTkFrame(root)
+    settings_frame.pack(pady=10, padx=10, fill="both", expand=True)
 
     settings = [
         ("Threshhold:", 0, 500),
@@ -10,9 +10,9 @@ def create_settings_frame(root):
     ]
 
     for i, (label_text, from_, to) in enumerate(settings):
-        label = ctk.CTkLabel(sensors_frame, text=label_text)
+        label = ctk.CTkLabel(settings_frame, text=label_text)
         label.grid(row=i, column=0, padx=5, pady=5)
-        slider = ctk.CTkSlider(sensors_frame, from_=from_, to=to)
+        slider = ctk.CTkSlider(settings_frame, from_=from_, to=to)
         slider.grid(row=i, column=1, padx=5, pady=5, sticky="ew")
 
-    return sensors_frame
+    return settings_frame
